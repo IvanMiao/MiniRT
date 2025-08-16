637cdd65e747bba5faf316861590c9d220645d30
Ivan Miao <reneisaipa@gmail.com>
2025-08-16 02:45:26 +0200

    add math expl.

--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -6,7 +6,7 @@
 /*   By: ymiao <ymiao@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 18:07:27 by ymiao             #+#    #+#             */
-/*   Updated: 2025/08/14 18:00:04 by ymiao            ###   ########.fr       */
+/*   Updated: 2025/08/16 01:57:46 by ymiao            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ double	hit_sphere(const t_sphere *sp, const t_ray *ray)
 }
 
 /**
- * @brief Calculates the surface normal vector at a given point on a sphere.
+ * @brief Calculates the surface normal vector *at* a given point on a sphere.
  * 
  * @param sp A pointer to the sphere struct.
  * @param p The point on the sphere's surface where the noraml is calculated.
